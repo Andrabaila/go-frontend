57bca9edb97f541303a2338d7f9df0fe88ce029f
Yury Andrabaila <yandrabaila@gmail.com>
2025-11-11 09:46:27 +0100

    fix: types imports

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,5 +1,5 @@
-export * from './base'; // базовые типы (Coordinates, Player, Quest)
-export * from './map'; // карта и GeoJSON
-export * from './ui'; // интерфейсные компоненты
+export * from './types/base'; // базовые типы (Coordinates, Player, Quest)
+export * from './types/map'; // карта и GeoJSON
+export * from './types/ui'; // интерфейсные компоненты
 export * from './types/api'; // API-ответы и запросы
-export * from './game'; // игровая логика
+export * from './types/game'; // игровая логика
