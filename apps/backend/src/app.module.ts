9e2fbd5ec3e8bbd38a0d3ed535689cf8f380285c
Yury Andrabaila <yandrabaila@gmail.com>
2026-01-19 12:31:10 +0100

    fix(backend): configure TypeORM to use DATABASE_URL and SSL for Railway

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,28 +1,30 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+
 import { PlayersModule } from './players/players.module.js';
 import { GameObjectsModule } from './game-objects/game-objects.module.js';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Player } from './players/player.entity.js';
 import { AuthModule } from './auth/auth.module.js';
 import { UsersModule } from './users/users.module.js';
-import { User } from './users/user.entity.js';
 import { TileModule } from './tiles/tile.module.js';
 import { QuestsModule } from './quests/quests.module.js';
+
+import { Player } from './players/player.entity.js';
+import { User } from './users/user.entity.js';
 import { Quest } from './quests/quest.entity.js';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'very-secret-password',
-      database: 'go_game',
+      url: process.env.DATABASE_URL,
       entities: [Player, User, Quest],
       synchronize: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     }),
     PlayersModule,
     GameObjectsModule,
@@ -31,7 +33,7 @@ import { Quest } from './quests/quest.entity.js';
     TileModule,
     QuestsModule,
   ],
-  controllers: [AppController], // <── UsersController УБРАТЬ
-  providers: [AppService], // <── UsersService УБРАТЬ
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
